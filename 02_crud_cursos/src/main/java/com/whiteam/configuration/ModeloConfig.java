bf5b2f392b82c62f7fbd22f50ab871f517d5ca69
USER <USER@DESKTOP-M50KV5N>
2022-02-17 08:16:39 +0100

    proyecto crud

---
+++ 02_crud_cursos/src/main/java/com/whiteam/configuration/ModeloConfig.java
@@ -0,0 +1,10 @@
+package com.whiteam.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan(basePackages = "com.whiteam.service")
+@Configuration
+public class ModeloConfig {	
+
+}
---
+++ 02_crud_cursos/src/main/java/com/whiteam/configuration/MvcConfig.java
@@ -0,0 +1,13 @@
+package com.whiteam.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@ComponentScan(basePackages = "com.whiteam.controller")
+@Configuration
+public class MvcConfig implements WebMvcConfigurer{
+		
+}
